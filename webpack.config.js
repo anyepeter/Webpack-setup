75c672c098bc8be7139fe43b3eb3a49a360fe13f
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-09-27 11:57:32 +0100

    Setup the wabpack include all the files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Wbpack Exercise',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  mode: "production"
+};
